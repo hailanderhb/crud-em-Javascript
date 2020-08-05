fcf839d9ee06b9b281962be44771962eacd0a3d3
Hailander Bastos <hailanderhb@gmail.com>
2020-08-05 06:44:48 -0300

    Um crud em Javascript puro no html

---
+++ script.js
@@ -0,0 +1,72 @@
+var linhaSelecionada = null;
+
+function enviarOn(){
+    var dadosFormulario = lerDados();
+    if(linhaSelecionada == null){
+        inserirNovosDados(dadosFormulario);
+    }else{
+        atualizarDados(dadosFormulario);
+    }
+    
+    resetarForm();
+}
+
+function lerDados(){
+    var dadosFormulario = {};
+    dadosFormulario["nomeCompleto"] = document.getElementById("nomeCompleto").value;
+    dadosFormulario["profissao"] = document.getElementById("profissao").value;
+    dadosFormulario["salario"] = document.getElementById("salario").value;
+    dadosFormulario["cidade"] = document.getElementById("cidade").value;
+    return dadosFormulario;
+}
+
+function inserirNovosDados(data){
+    var table = document.getElementById("listaEmpregados").getElementsByTagName('tbody')[0];
+    var novaLinha = table.insertRow(table.length);
+    cell1 = novaLinha.insertCell(0);
+    cell1.innerHTML = data.nomeCompleto;
+    cell2 = novaLinha.insertCell(1);
+    cell2.innerHTML = data.profissao;
+    cell3 = novaLinha.insertCell(2);
+    cell3.innerHTML = data.salario;
+    cell4 = novaLinha.insertCell(3);
+    cell4.innerHTML = data.cidade;
+    cell4 = novaLinha.insertCell(4);
+    cell4.innerHTML = `<a onclick= "editar(this)">Editar</a>/
+                        <a onclick= "deletar(this)">Deletar</a>`;
+
+}
+
+function resetarForm(){
+    document.getElementById("nomeCompleto").value = "";
+    document.getElementById("profissao").value = "";
+    document.getElementById("salario").value = "";
+    document.getElementById("cidade").value = "";
+    linhaSelecionada = null;
+}
+
+function editar(td){
+    linhaSelecionada = td.parentElement.parentElement;
+    document.getElementById("nomeCompleto").value = linhaSelecionada.cells[0].innerHTML;
+    document.getElementById("profissao").value = linhaSelecionada.cells[1].innerHTML;
+    document.getElementById("salario").value = linhaSelecionada.cells[2].innerHTML;
+    document.getElementById("cidade").value = linhaSelecionada.cells[3].innerHTML;
+
+}
+
+function atualizarDados(dadosFormulario){
+    linhaSelecionada.cells[0].innerHTML = dadosFormulario.nomeCompleto;
+    linhaSelecionada.cells[1].innerHTML = dadosFormulario.profissao;
+    linhaSelecionada.cells[2].innerHTML = dadosFormulario.salario;
+    linhaSelecionada.cells[3].innerHTML = dadosFormulario.cidade;
+
+}
+
+function deletar(td){
+    if(confirm("Tem certeza que quer deletar esses dados?")){
+        linha = td.parentElement.parentElement;
+        document.getElementById("listaEmpregados").deleteRow(linha.rowIndex);
+        resetarForm();
+    }
+
+}
